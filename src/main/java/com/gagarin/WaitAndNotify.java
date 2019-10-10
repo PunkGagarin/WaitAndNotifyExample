55ae449298cb85efaa28350b602ebe799623702f
Roman <41580710+PunkGagarin@users.noreply.github.com>
2019-10-10 16:56:12 +0300

    Wait and Notify example with Producer and Consumer pattern

---
+++ src/main/java/com/gagarin/WaitAndNotify.java
@@ -0,0 +1,69 @@
+package com.gagarin;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+public class WaitAndNotify {
+    public static void main(String[] args) {
+        ConsumerProducer cp = new ConsumerProducer();
+
+        Thread thread1 = new Thread(() -> {
+            try {
+                cp.produce();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                cp.consume();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+    }
+}
+
+class ConsumerProducer {
+    private Queue<Integer> queue = new LinkedList<>();
+    private final int LIMIT = 10;
+    private final Object lock = new Object();
+
+    public void produce() throws InterruptedException {
+        int value = 0;
+        while (true) {
+            synchronized (lock) {
+                while (queue.size() >= LIMIT)
+                    lock.wait();
+
+                System.out.println("Putting new Integer into the Queue");
+                System.out.println("");
+                queue.offer(new Random().nextInt(100));
+                lock.notify();
+            }
+        }
+    }
+
+    public void consume() throws InterruptedException {
+        while (true) {
+            synchronized (lock) {
+                while (queue.size() <= 0)
+                    lock.wait();
+
+                int value = queue.poll();
+                System.out.println("Getting value " + value + " from the queue");
+                System.out.println("Queue size: " + queue.size());
+                lock.notify();
+            }
+
+            Thread.sleep(1000);
+        }
+    }
+
+}
